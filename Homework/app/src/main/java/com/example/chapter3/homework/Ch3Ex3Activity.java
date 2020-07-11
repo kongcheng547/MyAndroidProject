60c39d5c43893e8d60336e72aed194a58135d561
kongcheng547 <3180103166@zju.edu.cn>
2020-07-11 14:23:01 +0800

    完成第三次作业

--- Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
@@ -1,5 +1,9 @@
 package com.example.chapter3.homework;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,17 +15,32 @@ import android.os.Bundle;
  */
 public class Ch3Ex3Activity extends AppCompatActivity {
 
+    private static final int PAGE_COUNT = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ch3ex3);
-
-
-
+        //setContentView(R.layout.activity_ch3ex3);
         // TODO: ex3-1. 添加 ViewPager 和 Fragment 做可滑动界面
+        setContentView(R.layout.activity_view_pager_with_tab);
+        ViewPager pager = findViewById(R.id.view_pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        pager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return new PlaceholderFragment();
+            }
 
+            @Override
+            public int getCount() {
+                return PAGE_COUNT;
+            }
 
-
-        // TODO: ex3-2, 添加 TabLayout 支持 Tab
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return "List " + position;
+            }
+        });
+        tabLayout.setupWithViewPager(pager);
     }
+        // TODO: ex3-2, 添加 TabLayout 支持 Tab
 }
--- Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
@@ -4,29 +4,44 @@ package com.example.chapter3.homework;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class PlaceholderFragment extends Fragment {
 
-
+    private LottieAnimationView lottieAnimationView;
+    private TextView textView;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO ex3-3: 修改 fragment_placeholder，添加 loading 控件和列表视图控件
+        //在xml中已经添加了
         return inflater.inflate(R.layout.fragment_placeholder, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
+        lottieAnimationView=getView().findViewById(R.id.lottie_anim);
+        textView=getView().findViewById(R.id.text_view_lll);
         getView().postDelayed(new Runnable() {
             @Override
             public void run() {
                 // 这里会在 5s 后执行
                 // TODO ex3-4：实现动画，将 lottie 控件淡出，列表数据淡入
+                lottieAnimationView.animate().alpha(0).setDuration(1000);
+                textView.animate().alpha(1).setDuration(1000);
             }
         }, 5000);
     }
